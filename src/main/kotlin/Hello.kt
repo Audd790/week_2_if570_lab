cd468bb78c4c0332e59a2d11e430d5cde54beddd
auddl <10horysheet10@gmail.com>
2023-02-16 10:46:40 +0700

    Learning more about functions

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,17 +1,27 @@
-fun main(args: Array<String>){
-        println("Hello, ${args[0]}")
-        println("=============================")
+import java.util.*
 
-        val isUnit = println("This is an expression")
-        println(isUnit)
-        println("=============================")
+fun randomDay() : String {
+        val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+                "Friday", "Saturday", "Sunday")
+        return week[Random().nextInt(week.size)]
+}
 
-        val temperature = 10
-        val isHot = if (temperature > 50) true else false
-        println(isHot)
+fun fishFood (day : String) : String {
+        return when (day) {
+                "Monday" -> "flakes"
+                "Wednesday" -> "redworms"
+                "Thursday" -> "granules"
+                "Friday" -> "mosquitoes"
+                "Sunday" -> "plankton"
+                else -> "nothing"
+        }
+}
 
-        println("=============================")
-        val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK"
-        }."
-        println(message)
+fun feedTheFish() {
+        val day = randomDay()
+        val food = fishFood(day)
+        println ("Today is $day and the fish eat $food")
+}
+fun main(args: Array<String>) {
+        feedTheFish()
 }
